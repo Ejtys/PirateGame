6c9cd7d633a9d52dd6f702b49f5207bcf0ea33b4
Ejtys <jwziebapn@gmail.com>
2023-08-09 17:37:21 +0200

    Add pan_input method

--- code/editor.py
+++ code/editor.py
@@ -1,15 +1,39 @@
 import pygame, sys
+from pygame.math import Vector2 as Vector
+from pygame.mouse import get_pressed as mouse_buttons
+from pygame.mouse import get_pos as mouse_pos
+
 from settings import *
 
 class Editor:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         
+        #navigation
+        self.origin = Vector( )
+        self.pan_active = False
+        self.pan_offset = Vector()
+    
+    #input
     def event_loop(self):
-        for even in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            self.pan_input(event)
+    
+    def pan_input(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
+            self.pan_active = True
+            self.pan_offset = Vector(mouse_pos()) - self.origin
+        if not mouse_buttons()[1]:
+            self.pan_active = False
+        
+        if self.pan_active:
+            self.origin = Vector(mouse_pos()) - self.pan_offset
     
     def run(self, dt):
+        self.display_surface.fill("white")
         self.event_loop( )
+        pygame.draw.circle(self.display_surface, "red", self.origin, 10)

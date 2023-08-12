dff3cd48b54a6c12312440337ee161429939dc5d
Ejtys <jwziebapn@gmail.com>
2023-08-12 10:59:47 +0200

    Add import_folder

--- code/support.py
+++ code/support.py
@@ -2,7 +2,15 @@ import pygame
 from os import walk
 
 def import_folder(path):
-    print(path)
+    surface_list = []
+    
+    for folder_name, sub_folder, image_files in walk(path):
+        for image_name in image_files:
+            fullpath = path + "/" +  image_name
+            image_surface = pygame.image.load(fullpath)
+            surface_list.append(image_surface)
+    
+    return surface_list
 
 def import_folder_dict(path):
     pass 
